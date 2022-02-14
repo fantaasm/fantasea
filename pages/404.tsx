de584b0f9ce893aa8be59a79e44b5bd19d771adc
Asperun <carnageepl@gmail.com>
2022-02-14 10:13:50 +0100

    Added 404 redirect page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+const errorPage = () => {
+  return null;
+}
+export const getStaticProps = () => {
+  return {
+    redirect: {
+      destination: '/',
+    },
+  };
+};
+
+export default errorPage
