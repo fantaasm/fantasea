702450f3e88589711c24442e0e3ce65dd1b505d9
Asperun <carnageepl@gmail.com>
2022-02-14 10:18:21 +0100

    Updated with technology render

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import {memo, ReactElement, useState} from 'react';
 import Image from "next/image";
+import {Technology} from "../utils/mappings";
 
 type Props = {
   title: string
   description: string
-  stack: string[]
+  stack: Technology[]
   bgImg: string | StaticImageData
-  children?: HTMLElement
 };
 
-function ProjectContainer(props: Props) : ReactElement {
+function ProjectContainer(props: Props): ReactElement {
   const [hovered, setHovered] = useState<boolean>(false);
+
+  function renderStack() {
+    return props.stack.map((technology: Technology, i: number) => {
+      return (<div key={i}
+                   className={"border-2 pl-1 pr-1 rounded-xl z-10 hover:rounded-none transition-all duration-300 ease-out"}
+                   style={{borderColor: technology.color, color: technology.color}}>{technology.text}</div>)
+    })
+  }
 
   return (
     <button className={"border-2 relative border-lightPurple h-96 w-80 hover:scale-110 hover:bg-lightPurple transition-all duration-200 ease-out overflow-hidden content-start text-center shadow-xl rounded-md hover:rounded-none"}
@@ -22,13 +30,12 @@ function ProjectContainer(props: Props) : ReactElement {
              src={props.bgImg}
              alt={""}
              layout={"fill"} />
-      <div className={"flex flex-col justify-center items-center h-full p-2"}>
+      <div className={"flex flex-col justify-center items-center h-full gap-4 p-2"}>
         {!hovered && props.title && <h1 className={"text-color-primary tracking-wider text-2xl"}>{props.title}</h1>}
         {hovered && props.description && <span className={"text-color-primary"}>{props.description}</span>}
-        {hovered && props.stack && <p>{props.stack.reduce((collector: string, next: string) => collector.concat(" " + next))}</p>}
+        {hovered && props.stack && <div className={"flex gap-2"}>{renderStack()}</div>}
       </div>
-    </button>
-  )
+    </button>)
 }
 
 export default memo(ProjectContainer);
