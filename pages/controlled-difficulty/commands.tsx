80fbbc3b81df17d60f548242224227c390a190a4
Asperun <carnageepl@gmail.com>
2022-02-17 17:25:56 +0100

    Removed unused code

--- pages/controlled-difficulty/commands.tsx
+++ pages/controlled-difficulty/commands.tsx
@@ -6,17 +6,18 @@ import headerPic from "../../public/cd-header.png";
 
 const CdCommands = () => {
   return (
-    <Article title={"Commands - Controlled Difficulty"}
-             header={"Controlled Difficulty"}
-             externalLinks={cdLinks}
-             internalLinks={[]}
-             active={cdLinks[1]}
-             image={headerPic}>
-
-      <h3>Command types</h3>
-      <h3>Chat commands</h3>
-      <h3>Console commands</h3>
-    </Article>
+    null
+    // <Article title={"Commands - Controlled Difficulty"}
+    //          header={"Controlled Difficulty"}
+    //          externalLinks={cdLinks}
+    //          internalLinks={[]}
+    //          active={cdLinks[1]}
+    //          image={headerPic}>
+    //
+    //   <h3>Command types</h3>
+    //   <h3>Chat commands</h3>
+    //   <h3>Console commands</h3>
+    // </Article>
   );
 }
 
