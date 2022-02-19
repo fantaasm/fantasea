d2bc8c080d07ff09cc7ef16d90ed88998d274327
Asperun <carnageepl@gmail.com>
2022-02-19 12:20:57 +0100

    Added footer as separate component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = (): JSX.Element => {
+  return (
+    <div className={"mt-4 mb-2 sm:mt-8 text-center text-slate-400"}>Copyright {new Date().getFullYear()} - Fantasm</div>
+  );
+}
+
+export default Footer
