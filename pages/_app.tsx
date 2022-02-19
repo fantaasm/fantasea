09fc64343995598e301ddd01b6b8e13ba00852d3
Asperun <carnageepl@gmail.com>
2022-02-19 12:22:19 +0100

    Updated with SEO and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,39 @@ import type {AppProps} from 'next/app'
 import {AnimatePresence} from "framer-motion";
 import Head from 'next/head'
 import React from "react";
+import {DefaultSeo} from "next-seo";
+import Footer from "../components/Footer";
+import FixedFadeToDark from "../components/FixedFadeToDark";
 
-// import { DefaultSeo } from 'next-seo'
 
 function MyApp({Component, pageProps, router}: AppProps) {
+  const url = `https://fantasm.vercel.app${router.route}`
 
   return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favIcon.png" />
+      </Head>
+      <DefaultSeo
+        titleTemplate="%s - Fantasm"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url,
+          description: 'The personal website for Fantasm, developer.',
+          site_name: 'Fantasm | fantasm.vercel.app',
+          images: [],
+        }}
+        canonical={url}
+      />
       <AnimatePresence exitBeforeEnter initial={true} onExitComplete={() => window.scrollTo(0, 0)}>
-        <Head><link rel="icon" type="image/png" href="/favIcon.png" /></Head>
-        <Component {...pageProps} key={router.pathname} />
+        <Component {...pageProps} canonical={url} key={router.pathname} />
       </AnimatePresence>
+      <Footer/>
+      <FixedFadeToDark/>
+    </>
   )
+
 }
 
 export default MyApp
