5b58806dc6b7f9029c6bcc429aa1ecde9bd052f5
Asperun <carnageepl@gmail.com>
2022-02-21 09:03:45 +0100

    Added padding

--- components/ArticleLinks.tsx
+++ components/ArticleLinks.tsx
@@ -10,25 +10,22 @@ type Props = {
   internal?: boolean
 }
 
-const ArticleLinks = ({links, className, active, children,internal}: Props): JSX.Element => {
+const ArticleLinks = ({links, className, active, children, internal}: Props): JSX.Element => {
   return (
     <div className={className}>
       <div className={`flex flex-col border-slate-500 ${active && "border-l"} gap-4 sm:gap-2`}>
         {children}
-        {links.map((articleLink: ArticleLink, i: number) =>
-          internal
-          &&
-            <a key={i} href={articleLink.link} className={`hover:text-slate-300 text-slate-400 -ml-px pl-4 border-slate-500 hover:border-slate-300 ${active && "border-l "} ${active === articleLink.name && "border-sky-500 text-sky-500"}`}>{articleLink.name} </a>
-          ||
-            <Link key={i}
-                  href={"/blog/" + articleLink.link}>
-              <a className={`hover:text-slate-300 text-slate-400 -ml-px pl-4 border-slate-500 hover:border-slate-300 ${active && "border-l "} ${active === articleLink.name && "border-sky-500 text-sky-500"}`}>{articleLink.name} </a>
+        {links.map((articleLink: ArticleLink, i: number) => {
+            if (internal) {
+              return <a key={i} href={articleLink.link} className={`hover:text-slate-300 text-slate-400 -ml-px pl-4 border-slate-500 hover:border-slate-300 ${active && "border-l "} ${active === articleLink.name && "border-sky-500 text-sky-500"}`}>{articleLink.name} </a>
+            }
+            return <Link key={i} href={"/blog/" + articleLink.link}>
+              <a className={`hover:text-slate-300 hover:border-slate-300 text-slate-400 -ml-px pl-4 px-1 border-slate-500 ${active && "border-l"} ${active === articleLink.name && "border-sky-500 text-sky-500"}`}>{articleLink.name} </a>
             </Link>
+          }
         )}
       </div>
     </div>
   );
 }
 export default ArticleLinks
-
-
