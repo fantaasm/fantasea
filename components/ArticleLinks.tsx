eecfbeeca7b30422db8d6ccb4f29ae5ffe7eba58
Asperun <carnageepl@gmail.com>
2022-04-11 11:20:09 +0200

    Updated props to handle all passed props

--- components/ArticleLinks.tsx
+++ components/ArticleLinks.tsx
@@ -1,29 +1,22 @@
 import Link from "next/link";
-import { ArticleLink } from "../types/types";
+import { ArticleLink } from "../types";
 
-type Props = {
-  className?: string;
+interface Props extends React.HTMLAttributes<HTMLElement> {
   links: ArticleLink[];
   active?: string;
-};
+}
 
-const ArticleLinks = ({ links, className, active }: Props): JSX.Element => {
+const ArticleLinks = ({ links, active, ...props }: Props): JSX.Element => {
   return (
-    <div className={className}>
-      <div
-        className={`flex flex-col border-slate-500 ${
-          active && "border-l"
-        } gap-4 sm:gap-2`}
-      >
+    <aside {...props}>
+      <div className={`flex flex-col border-slate-500 ${active && "border-l"} gap-4 sm:gap-2`}>
         {links.map((articleLink: ArticleLink, i: number) => {
           return (
             <Link key={i} href={"/blog/" + articleLink.slug}>
               <a
                 className={`hover:text-slate-300 hover:border-slate-300 text-slate-400 -ml-px pl-4 px-1 border-slate-500 ${
                   active && "border-l"
-                } ${
-                  active === articleLink.title && "border-sky-500 text-sky-500"
-                }`}
+                } ${active === articleLink.title && "border-sky-500 text-sky-500"}`}
               >
                 {articleLink.title}{" "}
               </a>
@@ -31,7 +24,7 @@ const ArticleLinks = ({ links, className, active }: Props): JSX.Element => {
           );
         })}
       </div>
-    </div>
+    </aside>
   );
 };
 export default ArticleLinks;
