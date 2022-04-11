f5225744aaa74e0a03f137d923e89a694d002e57
Asperun <carnageepl@gmail.com>
2022-04-11 11:21:12 +0200

    Removed unused elements

--- components/FixedFadeToDark.tsx
+++
@@ -1,8 +0,0 @@
-const FixedFadeToDark = (): JSX.Element => {
-  return (
-    <div className="h-8 pointer-events-none fixed bottom-0 w-full bg-gradient-to-t from-dark" />
-    // <div className="h-8 pointer-events-none fixed bottom-0 w-full bg-gradient-to-t from-gray-800" />
-  );
-};
-
-export default FixedFadeToDark;
