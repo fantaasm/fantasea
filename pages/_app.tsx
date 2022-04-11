87f4bff6e37e959d9a8645b3e3d9508f9876e97e
Asperun <carnageepl@gmail.com>
2022-04-11 11:18:26 +0200

    Added web vitals, removed unused components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,35 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import {AnimatePresence} from "framer-motion";
 import Head from "next/head";
-import React from "react";
 import { DefaultSeo } from "next-seo";
-import Footer from "../components/Footer";
-import FixedFadeToDark from "../components/FixedFadeToDark";
+
+export let reportWebVitals: void;
+
+if (process.env.NODE_ENV === "development") {
+  // @ts-ignore
+  reportWebVitals = (metrics) => {
+    console.log(metrics);
+  };
+}
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-  const url = process.env.NEXT_PUBLIC_DOMAIN_NAME +router.asPath;
+  const url = process.env.NEXT_PUBLIC_DOMAIN_NAME + router.asPath;
 
   return (
     <>
       <Head>
-        <link rel="icon" type="image/png" href="/favIcon.png" />
+        <link rel="icon" type="image/webp" href="/favicon.webp" />
       </Head>
       <DefaultSeo
-        defaultTitle="Fantasea"
         openGraph={{
           type: "website",
-          locale: "en_IE",
+          locale: "en_US",
           url,
-          description: "The personal website for Fantasm, developer.",
-          site_name: "Fantasm | fantasea.pl",
-          images: [],
+          site_name: "Fantasea",
         }}
         canonical={url}
       />
-      <AnimatePresence exitBeforeEnter initial={true} onExitComplete={() => window.scrollTo(0, 0)}>
-        <Component {...pageProps} canonical={url} key={router.pathname} />
-      </AnimatePresence>
-      <Footer />
-      <FixedFadeToDark />
+      <Component {...pageProps} canonical={url} key={router.pathname} />
     </>
   );
 }
