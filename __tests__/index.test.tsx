45edd0f60bbc62925cfa12c1a4f154620a0313fd
Asperun <carnageepl@gmail.com>
2022-04-11 11:16:32 +0200

    Updated tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,38 +1,8 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import SocialLinks from "../components/SocialLinks";
 import userEvent from "@testing-library/user-event";
 
 describe("Global", () => {
   it(".env exposed", () => {
     expect(process.env.NEXT_PUBLIC_GRAPHCMS_API_ENDPOINT).not.toBeUndefined();
     expect(process.env.NEXT_PUBLIC_GRAPHCMS_API_ENDPOINT).not.toBeNull();
-  });
-});
-
-describe("Social Links", () => {
-  it("Buttons text", () => {
-    render(<SocialLinks />);
-
-    const emailText = screen.getByText("carnageepl@gmail.com");
-    const discordText = screen.getByText("fantasm#9591");
-    const githubText = screen.getByText("Github");
-
-    expect(discordText).toBeInTheDocument();
-    expect(emailText).toBeInTheDocument();
-    expect(githubText).toBeInTheDocument();
-  });
-
-  it("Buttons Interactivity", () => {
-    render(<SocialLinks />);
-
-    const buttons = screen.getAllByRole("button");
-
-    expect(buttons).not.toBeUndefined();
-
-    const emailButton = buttons[0];
-
-    userEvent.click(emailButton);
-
-    expect(emailButton).toHaveTextContent("Copied to clipboard!");
   });
 });
